9c8165999c4e6c1fc03a09bfbc9bbead509058cb
QSD_o <info@qsdbih.com>
2020-09-28 18:55:28 +0200

    Initial commit

---
+++ CryptoPriceViewer/CryptoPriceViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPriceViewerApp.swift
+//  CryptoPriceViewer
+//
+//  Created by Omer Rahmanovic on 9/28/20.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPriceViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CoinList()
+        }
+    }
+}
---
+++ CryptoPriceViewer/CryptoService.swift
@@ -0,0 +1,45 @@
+//
+//  CryptoService.swift
+//  CryptoPriceViewer
+//
+//  Created by Omer Rahmanovic on 9/28/20.
+//
+
+import Foundation
+import Combine
+
+final class CryptoService {
+    
+    var components: URLComponents {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.coinranking.com"
+        components.path = "/v1/public/coins"
+        components.queryItems = [URLQueryItem(name: "base", value: "USD"), URLQueryItem(name: "timePeriod", value: "24h")]
+        return components
+    }
+    
+    func fetchCoins() -> AnyPublisher<CryptoDataContainer, Error> {
+        return URLSession.shared.dataTaskPublisher(for: components.url!)
+            .map { $0.data }
+            .decode(type: CryptoDataContainer.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+}
+
+struct CryptoDataContainer: Decodable {
+    let status: String
+    let data: CryptoData
+    
+}
+
+struct CryptoData: Decodable {
+    let coins: [Coin]
+}
+
+struct Coin: Decodable, Hashable {
+    let name: String
+    let price: String
+}
